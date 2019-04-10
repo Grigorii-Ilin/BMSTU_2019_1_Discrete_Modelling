853fc7d4a9406cec822b0cc9462dac4c144254fa
Grigorii Ilin <37904148+Grigorii-Ilin@users.noreply.github.com>
2019-04-10 12:25:03 +0300

    added scheme image and code cleare

--- queue_time_and_event_modelling/gui.py
+++ queue_time_and_event_modelling/gui.py
@@ -2,22 +2,25 @@ import sys
 import functools
 import itertools
 
-from PyQt5.QtWidgets import QApplication, QMainWindow,  QPushButton,  QLabel, QErrorMessage, QMessageBox, QTableWidget#,QTableWidgetItem
+from PyQt5.QtWidgets import QApplication, QMainWindow,  QPushButton,  QLabel, QErrorMessage, QMessageBox, QTableWidget
 from PyQt5.QtCore import  QSize
+from PyQt5.QtGui import QPixmap
+
 import matplotlib.pyplot as plt
 
-#from pprint import pprint
 import delta_time_modelling
 import events_modelling
 
 class MyWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)       
-        self.setMinimumSize(QSize(640, 300))   
+        self.setMinimumSize(QSize(800, 300))   
 
         self.create_table()
+        self.create_image()
         self.create_main_button()
         self.create_about()
+
 
     def create_table(self):
         self.tbl_input = QTableWidget(self)
@@ -31,6 +34,14 @@ class MyWindow(QMainWindow):
         self.tbl_input.setVerticalHeaderLabels(headers_vert)
 
         self.tbl_input.resize(220,210)
+
+
+    def create_image(self):
+        self.lbl_scheme_image = QLabel(self)
+        pixmap=QPixmap('scheme.png')
+        self.lbl_scheme_image.setPixmap(pixmap)
+        self.lbl_scheme_image.move(225, 0)
+        self.lbl_scheme_image.resize(pixmap.width(), pixmap.height())
 
 
     def create_main_button(self):
@@ -55,49 +66,31 @@ class MyWindow(QMainWindow):
     
     @staticmethod
     def show_max_memory_size(m_dt, m_e):
-        #m_dt=model_result['delta_time']['memory_blocks_max']
-        #m_e=model_result['events']['memory_blocks_max']
-
         msg = QMessageBox(
             QMessageBox.Information,
             'Успех!',
             'Максимальный объем памяти: \nв Δt алгоритме: {0:d}, \nв событийном алгоритме: {1:d}.'.format(m_dt, m_e), 
             QMessageBox.Ok)
         msg.exec_() 
-    
+ 
+        
     @staticmethod
     def show_diagrams(model_result):
-
-        #def one_diag(index, title, model_result_current):
-        #    diagrams[index].set_title(title)
-        #    diagrams[index].plot(model_result_current['times'], model_result_current['memory_blocks_count']) 
-
         figure, diagrams = plt.subplots(2, 1)
         figure.suptitle('Использование памяти в Δt алгоритме и событийном алгоритме')
 
-        #index_increment = functools.partial(next, itertools.count())
-        #one_diag(index_increment(), 'Δt алгоритм', model_result['delta_time'])
-        #one_diag(index_increment(), '\nСобытийный алгоритм', model_result['events'])
-        
-        #diag_indexes=range(0,2)
-        #diag_titles=['Δt алгоритм', 'Событийный алгоритм']
-        #model_result_keys=['delta_time', 'events']
-        #for diag_index, diag_title, model_result_key in zip(diag_indexes, diag_titles, model_result_keys):
-        #    pass
-
-        #diagrams[0].set_title('Δt алгоритм')
         diagrams[0].plot(model_result['delta_time']['times'], model_result['delta_time']['memory_blocks_count']) 
-        
-        #diagrams[1].set_title('Событийный алгоритм')
         diagrams[1].plot(model_result['events']['times'], model_result['events']['memory_blocks_count']) 
 
         plt.show() 
+
 
     def read_cell(self, row, left_border=0.0, right_border=sys.float_info.max):
         cell_item = self.tbl_input.item(row, 0)
         result = float(cell_item.text()) if cell_item else 0.0
         assert(result >= left_border and result <= right_border)
         return result
+
 
     def calc(self):
         try:
@@ -122,38 +115,7 @@ class MyWindow(QMainWindow):
         self.show_diagrams(model_result)
 
 
-        #"""
-        #t = times, 
-        #q = memory_blocks_count, 
-        #m = memory_blocks_max
-        #_dt = delta_time_modelling
-        #_e = events_modelling
-        #"""
-        #t_dt, q_dt, m_dt=delta_time_modelling.model(a,
-        #                                            b,
-        #                                            lmbd,
-        #                                            dt,
-        #                                            t_end,
-        #                                            reentry_prob)
-
-        #t_e, q_e, m_e=delta_time_modelling.model(a,
-        #                                        b,
-        #                                        lmbd,
-        #                                        dt,
-        #                                        t_end,
-        #                                        reentry_prob)
-
-#t, q, m = delta_time_modelling.model(2, 5, 1.0, 1.0, 200.0, 0.8)
-#pprint(t, compact=True)
-#pprint(q, compact=True)
-#print(m)
-
-#t, q, m = events_modelling.model(2, 5, 1.0, 1.0, 200.0, 0.8)
-#pprint(t, compact=True)
-#pprint(q, compact=True)
-#print(m)
 app = QApplication(sys.argv)
 main_window = MyWindow()  
 main_window.show()
-fin = app.exec_()
-print(fin)
+app.exec_()
